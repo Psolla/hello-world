f3041f27fb25622bd114df300fb65bf2efd07fb1
Pedro Henrique Solla Nunes <90543058+Psolla@users.noreply.github.com>
2022-03-27 16:33:51 -0300

    FirstVaribleProjectInPhp.php
    I'm learning to use php and this is my first project I did about variables in php

---
+++ FirstProjectInPHP.php
@@ -0,0 +1,41 @@
+<?php
+$nome = "Variável Exemplo";
+
+echo $nome;
+
+$nome = 'Variável PHP';
+
+echo "\n";
+
+echo $nome;
+
+$numero= 5000;
+
+echo "\n";
+
+echo $numero;
+
+echo "\n";
+
+$nome1 = "Nome1.opgpg";
+
+echo $nome1;
+
+echo "\n";
+
+$primeiro_nome = "SOlla";
+
+echo $primeiro_nome;
+
+$VariavelTrue = true; //Valores booleano
+$VariavelFalse = false; //se não colocar nenhum número, ao executar o comando no cmd não vai aparecer nada.
+
+echo "\n";
+echo $VariavelTrue;
+echo "\n";
+echo $VariavelFalse; 
+
+
+//Fim do 3 exmeplo de PHP //Variáveis
+
+//First variable project in PHP
